a98f1e8613ce9a0ab20f35f36f481a85d7ae87b6
JeffreyLee <jeffreyleeon@gmail.com>
2015-04-14 13:16:20 +0800

    Add the relationship between person and video successfully

---
+++ db/migrate/20150414051255_add_person_id_to_videos.rb
@@ -0,0 +1,4 @@
+class AddPersonIdToVideos < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414044756) do
+ActiveRecord::Schema.define(version: 20150414051255) do
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "videos", force: :cascade do |t|
     t.string   "title"
