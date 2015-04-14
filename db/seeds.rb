bfa3cef9fb6e2e557e027a81ed71c2c28add929e
JeffreyLee <jeffreyleeon@gmail.com>
2015-04-14 18:16:55 +0800

    Adding seed videos for displaying

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+v = Video.new(
+  title: 'Steve Jobs\' 2005 Stanford Commencement Address',
+  link: 'https://www.youtube.com/watch?v=UF8uR6Z6KLc',
+  approved: true
+)
+v.save!
+
+v1 = Video.new(
+  title: 'Steve Jobs and Bill Gates Face Off',
+  link: 'https://www.youtube.com/watch?v=Sw8x7ASpRIY',
+  approved: true
+)
+v1.save!
+
+v2 = Video.new(
+  title: 'Steve Jobs iPhone 2007 Presentation',
+  link: 'https://www.youtube.com/watch?v=vN4U5FqrOdQ',
+  approved: false
+)
+v2.save!
